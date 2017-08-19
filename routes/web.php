ada855d9589debf742d07a6837d25baf6bececd4
JOB <job.125@hotmail.com>
2017-08-20 01:28:44 +0700

    test
    ttest

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('shop.index');
+
+ 
+});
+Route::get('/dress',function(){
+	return view('shop.dresses');	
+});
+
+
+Route::get('/create','ShopinterfaceController@index')->name('createselling');
+
+Auth::routes();
+Route::resource('/Home', 'ShopControllre');
+
